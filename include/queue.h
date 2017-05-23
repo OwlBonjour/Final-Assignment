6145d4c3880691e4ea94baad8ccbfb315cfc8163
OwlBonjour <OwlBonjour@users.noreply.github.com>
2017-05-23 22:41:03 +0800

    Update queue.h

--- include/queue.h
+++ include/queue.h
@@ -1,1 +1,25 @@
+#define QUEUE_INIT_SIZE 100
+#define QUEUEINCREMENT 10
 
+typedef struct QNode {
+	char data;
+	struct QNode *next;
+}QNode;
+typedef struct {
+	QNode *front, *rear;
+	int queuesize;
+}LinkQueue;
+
+#ifndef _QUEUE_H
+#define _QUEUE_H
+
+void InitQueue(LinkQueue *Q);
+void EnQueue(LinkQueue *Q, char e);
+int QueueisEmpty(LinkQueue Q);
+int QueueLength(LinkQueue Q);
+void DeQueue(LinkQueue *Q, char *e);
+void GetHead(LinkQueue Q, char *e);
+void DestroyQueue(LinkQueue *Q);
+void ClearQueue(LinkQueue *Q);
+
+#endif //_QUEUE_H
