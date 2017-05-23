fe684b0d699967251feff7f5ecdd565a0ccf0443
OwlBonjour <OwlBonjour@users.noreply.github.com>
2017-05-23 22:41:28 +0800

    Update stack.h

--- include/stack.h
+++ include/stack.h
@@ -1,1 +1,22 @@
+#define STACK_INIT_SIZE 100
+#define STACKINCREMENT 10
 
+typedef struct {
+	char *base;
+	char *top;
+	int stacksize;
+}SqStack;
+
+#ifndef _STACK_H
+#define _STACK_H
+
+void InitStack(SqStack *S);
+void DestroyStack(SqStack *S);
+void ClearStack(SqStack *S);
+int StackisEmpty(SqStack S);
+int StackLength(SqStack S);
+void GetTop(SqStack S, char *e);
+void Push(SqStack *S, char e);
+void Pop(SqStack *S, char *e);
+
+#endif //_STACK_H
